85a183e383de58ee364b9a1c42f83633a9ee65cf
Montekar <mantas.pasakarnis@gmail.com>
2020-11-29 15:20:57 +0100

    dal sql connection test

--- src/dal/CategoriesDAO.java
+++ src/dal/CategoriesDAO.java
@@ -1,4 +1,22 @@
 package dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+import java.io.IOException;
+import java.util.List;
+
 public class CategoriesDAO {
+
+    SQLServerDataSource ds;
+
+    public CategoriesDAO() throws IOException {
+        this.ds = new SQLServerDataSource();
+        DataBaseConnectionDAO connectionInfo = new DataBaseConnectionDAO();
+        List<String> infoList = connectionInfo.getDatabaseInfo();
+        ds.setDatabaseName(infoList.get(0));
+        ds.setUser(infoList.get(1));
+        ds.setPassword(infoList.get(2));
+        ds.setPortNumber(Integer.parseInt(infoList.get(3)));
+        ds.setServerName(infoList.get(4));
+    }
 }
--- src/dal/DataBaseConnectionDAO.java
+++ src/dal/DataBaseConnectionDAO.java
@@ -1,4 +1,36 @@
 package dal;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataBaseConnectionDAO {
+
+    /*
+    Gets database information from file. Adds it to an array to be sent to all DAO classes
+    so database connection could be easily possible
+    */
+
+    public List<String> getDatabaseInfo() throws IOException {
+        List<String> info = new ArrayList();
+        String source = "data/login.txt";
+        File file = new File(source);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    try {
+                        info.add(line);
+                    } catch (Exception ex) {
+                        System.out.println(ex);
+                    }
+                }
+            }
+        }
+        return info;
+    }
 }
